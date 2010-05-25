9131f8b999d81d02d33b5850ef5909b33b2a0773
yoda <yoda@tallon4.net>
2010-05-25 14:36:14 +0800

    Working on lab6

--- AllLabs/bezier.m
+++ AllLabs/bezier.m
@@ -1,4 +1,4 @@
-function P = bezier(t, CP)
+function P = bezier(varargin)
 %
 % bezier evaluates a point along a Bezier curve
 %
@@ -13,4 +13,57 @@ function P = bezier(t, CP)
 %
 % Returns
 % P  a 1xm vector   eg [x,y,z] giving the point on the curve
+
+p = inputParser;
+
+
+% All numerics are valid
+p.addRequired('t', @(a)isnumeric(a) && a >= 0 && a <= 1);
+p.addRequired('CP', @(a)size(a, 1) > 0 && size(a, 2) > 0 && isnumeric(a));
+
+try
+    % Do the validation of the parameters
+    p.parse(varargin{:});
+catch exception
+     disp(exception.identifier); % Debug catching correct errors
+     rethrow(exception)
+    % Only three numeric
+    if strcmp(exception.identifier, 'MATLAB:InputParser:UnmatchedParameter')
+        warning('bezier takes 2 parameters')
+    end
+    % Does not allow parameters (inputParser parameters)
+    if strcmp(exception.identifier, 'MATLAB:InputParser:MustBeChar')
+        error('Bad arguement, must be a numeric symbol')
+    end
+    % Does not allow parameters (inputParser parameters)
+    if strcmp(exception.identifier, 'MATLAB:InputParser:ParamMissingValue')
+        error('Bad arguement, must be a numeric symbol')
+    end
+    % Must have a numeric parameter
+    if strcmp(exception.identifier, 'MATLAB:minrhs')
+        error('bezier takes 2 parameters')
+    end 
+end
+t = p.Results.t;
+CP = p.Results.CP;
+
+[n, m] = size(CP);
+disp('n =');
+disp(n);
+disp(m);
+
+N = nchoosek(n, m);
+A = [];
+x = 1;
+    for i = 0: m,
+        
+            A(i+1, 1) = nchoosek(n, i) * (1-t)^(N-i) * t^i * CP(i+1, 1);
+            A(i+1, 2) = nchoosek(n, i) * (1-t)^(N-i) * t^i * CP(i+1, 2);
+            %A(i+1, 3) = nchoosek(n, i) * (1-t)^(N-i) * t^i * CP(i+1, 3);
+            
+    end
+
+disp(A);
+P = sum(A);
+
 end
---
+++ AllLabs/plotbezier.m
@@ -0,0 +1,60 @@
+function plotbezier(varargin)
+%  Arguments:
+%  dt   - the parameter increment size that you want points along the 
+%         curve to be plotted at.  Remember the parameter varies from 
+%         0 - 1 along the curve, dt = .01 will give 100 points
+%  CP   - a nxm vector of n control points in 2 or 3-space 
+%
+%  Produces:
+%  - a plot of the characteristic polygon
+%  - a plot of the curve as a series of equi-spaced points in terms of
+%    the curve parameter t.
+%  - a separate plot of velocity variation as a function of the parameter t
+p = inputParser;
+
+
+% All numerics are valid
+p.addRequired('dt', @(a)isnumeric(a) && a >= 0 && a <= 1);
+p.addRequired('CP', @(a)size(a, 1) > 0 && size(a, 2) > 0 && isnumeric(a));
+
+try
+    % Do the validation of the parameters
+    p.parse(varargin{:});
+catch exception
+     disp(exception.identifier); % Debug catching correct errors
+     rethrow(exception)
+    % Only three numeric
+    if strcmp(exception.identifier, 'MATLAB:InputParser:UnmatchedParameter')
+        warning('plotbezier takes 2 parameters')
+    end
+    % Does not allow parameters (inputParser parameters)
+    if strcmp(exception.identifier, 'MATLAB:InputParser:MustBeChar')
+        error('Bad arguement, must be a numeric symbol')
+    end
+    % Does not allow parameters (inputParser parameters)
+    if strcmp(exception.identifier, 'MATLAB:InputParser:ParamMissingValue')
+        error('Bad arguement, must be a numeric symbol')
+    end
+    % Must have a numeric parameter
+    if strcmp(exception.identifier, 'MATLAB:minrhs')
+        error('plotbezier takes 2 parameters')
+    end 
+end
+dt = p.Results.dt;
+CP = p.Results.CP;
+
+clf;
+
+axis equal;                            % make x y and z tick sizes equal
+grid on;
+hold on;
+
+plot(CP(:,1), CP(:,2), 'X');
+x = 0;
+while x <= 1,
+    P = bezier(x, CP);
+    plot(P(:, 2), P(:, 1), '.');
+    x = x + dt;
+end
+
+end
